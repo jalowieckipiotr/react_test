a93fc714a3776573f488fbde0336022c0c58f724
Piotr Jałowiecki <piotr.jalowiecki@certusoft.pl>
2020-09-15 08:56:51 +0200

    Updates

---
+++ precache-manifest.c036c80d621e8d6fa1645cc7d569f4d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ac36e794e6d84b331f4c780ac6008e",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "f85e7d80337b6246de02",
+    "url": "/react_test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3cd7fc42a0fe19fb4909",
+    "url": "/react_test/static/js/2.e52594f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_test/static/js/2.e52594f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85e7d80337b6246de02",
+    "url": "/react_test/static/js/main.9c63312b.chunk.js"
+  },
+  {
+    "revision": "2f31072355967d0e68ed",
+    "url": "/react_test/static/js/runtime-main.eb63a089.js"
+  }
+]);
