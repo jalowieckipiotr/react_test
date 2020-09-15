7b027722693254d52e9297780d2f0b36a87335b9
Piotr Jałowiecki <piotr.jalowiecki@certusoft.pl>
2020-09-15 08:53:13 +0200

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import UsersList from './UsersList'
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('includes input', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<input />)).toEqual(true)
+});
+
+it('includes UsersList', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<UsersList />)).toEqual(true)
+});
